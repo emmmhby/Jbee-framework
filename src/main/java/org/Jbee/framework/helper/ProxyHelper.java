953408b3d10fda13dcf0b88ac3084cd71a5b355d
Administrator <guethby@hotmail.com>
2017-11-22 23:53:05 +0800

    Dao层接口的相关代理类

--- src/main/java/org/Jbee/framework/helper/ProxyHelper.java
+++ src/main/java/org/Jbee/framework/helper/ProxyHelper.java
@@ -1,17 +1,27 @@
 package org.Jbee.framework.helper;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.Jbee.framework.orm.mapper.MapperProxy;
+import org.Jbee.framework.orm.mapper.MapperRegistry;
+import org.Jbee.framework.orm.session.SqlSession;
+import org.Jbee.framework.util.ClassUtil;
+import org.Jbee.framework.util.CollectionUtil;
 
-public class ProxyHelper implements InvocationHandler{
-    private Class<?> target;
+import java.util.Set;
 
-    public ProxyHelper(Class<?> target) {
-        this.target = target;
-    }
+public class ProxyHelper {
+    static {
+        Set<Class<?>> mapperClassSet =  ClassHelper.getRepositoryClassSet();
+        if(CollectionUtil.isNotEmpty(mapperClassSet)){
+            for (Class<?> mapperClass :
+                    mapperClassSet) {
+                try {
+                    MapperRegistry.addMapper(mapperClass);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        }
     }
 }
